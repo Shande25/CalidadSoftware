3508def8ccdc5ff19f112ca6e51a2fa78f8343a6
JonnyZ624 <tapiajonathan624@gmail.com>
2025-02-21 09:12:06 -0500

    Actividad

--- test_busqueda.py
+++ test_busqueda.py
@@ -5,13 +5,17 @@ arr_desordenado = [64, 34, 25, 12, 22, 11, 90]
 arr_ordenado = sorted(arr_desordenado)
 arr_vacio = []
 
+# Función genérica para verificar si el elemento está presente o no
+def verificar_elemento(arr, elemento, metodo_busqueda):
+    return metodo_busqueda(arr, elemento) != -1
+
 @pytest.mark.parametrize("arr, elemento, esperado", [
     (arr_desordenado, 22, True),  
     (arr_desordenado, 100, False),  
     (arr_vacio, 22, False)  
 ])
 def test_busqueda_lineal(arr, elemento, esperado):
-    assert (busqueda_lineal(arr, elemento) != -1) == esperado
+    assert verificar_elemento(arr, elemento, busqueda_lineal) == esperado
 
 @pytest.mark.parametrize("arr, elemento, esperado", [
     (arr_ordenado, 22, True),  
@@ -19,7 +23,7 @@ def test_busqueda_lineal(arr, elemento, esperado):
     (arr_vacio, 22, False)  
 ])
 def test_busqueda_binaria_iterativa(arr, elemento, esperado):
-    assert (busqueda_binaria_iterativa(arr, elemento) != -1) == esperado
+    assert verificar_elemento(arr, elemento, busqueda_binaria_iterativa) == esperado
 
 @pytest.mark.parametrize("arr, elemento, esperado", [
     (arr_ordenado, 22, True),  
@@ -27,4 +31,4 @@ def test_busqueda_binaria_iterativa(arr, elemento, esperado):
     (arr_vacio, 22, False)  
 ])
 def test_busqueda_binaria_recursiva(arr, elemento, esperado):
-    assert (busqueda_binaria_recursiva(arr, elemento, 0, len(arr) - 1) != -1) == esperado
+    assert verificar_elemento(arr, elemento, busqueda_binaria_recursiva) == esperado
--- test_ordenamiento.py
+++ test_ordenamiento.py
@@ -2,69 +2,47 @@ import pytest
 from ordenamiento import bubble_sort, seleccion_sort, insertion_sort, merge_sort, quick_sort
 
 # Bubble Sort Tests
-def test_bubble_sort_case1():
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    assert bubble_sort(arr[:]) == [11, 12, 22, 25, 34, 64, 90]
-
-def test_bubble_sort_case2():
-    arr = [5, 4, 3, 2, 1]
-    assert bubble_sort(arr[:]) == [1, 2, 3, 4, 5]
-
-def test_bubble_sort_case3():
-    arr = [3, 1, 2, 3, 1, 2]
-    assert bubble_sort(arr[:]) == [1, 1, 2, 2, 3, 3]
+@pytest.mark.parametrize("arr, esperado", [
+    ([64, 34, 25, 12, 22, 11, 90], [11, 12, 22, 25, 34, 64, 90]),
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
+    ([3, 1, 2, 3, 1, 2], [1, 1, 2, 2, 3, 3])
+])
+def test_bubble_sort(arr, esperado):
+    assert bubble_sort(arr[:]) == esperado
 
 # Selection Sort Tests
-def test_seleccion_sort_case1():
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    assert seleccion_sort(arr[:]) == [11, 12, 22, 25, 34, 64, 90]
-
-def test_seleccion_sort_case2():
-    arr = []
-    assert seleccion_sort(arr[:]) == []
-
-def test_seleccion_sort_case3():
-    arr = [5]
-    assert seleccion_sort(arr[:]) == [5]
+@pytest.mark.parametrize("arr, esperado", [
+    ([64, 34, 25, 12, 22, 11, 90], [11, 12, 22, 25, 34, 64, 90]),
+    ([], []),
+    ([5], [5])
+])
+def test_seleccion_sort(arr, esperado):
+    assert seleccion_sort(arr[:]) == esperado
 
 # Insertion Sort Tests
-def test_insertion_sort_case1():
-    arr = [1, 2, 3, 4, 5]
-    assert insertion_sort(arr[:]) == [1, 2, 3, 4, 5]
-
-def test_insertion_sort_case2():
-    arr = [5, 4, 3, 2, 1]
-    assert insertion_sort(arr[:]) == [1, 2, 3, 4, 5]
-
-def test_insertion_sort_case3():
-    arr = [3, -1, 2, -3, 1, 0]
-    assert insertion_sort(arr[:]) == [-3, -1, 0, 1, 2, 3]
+@pytest.mark.parametrize("arr, esperado", [
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
+    ([3, -1, 2, -3, 1, 0], [-3, -1, 0, 1, 2, 3])
+])
+def test_insertion_sort(arr, esperado):
+    assert insertion_sort(arr[:]) == esperado
 
 # Merge Sort Tests
-def test_merge_sort_case1():
-    arr = [64, 34, 25, 12, 22, 11, 90]
+@pytest.mark.parametrize("arr, esperado", [
+    ([64, 34, 25, 12, 22, 11, 90], [11, 12, 22, 25, 34, 64, 90]),
+    ([], []),
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])
+])
+def test_merge_sort(arr, esperado):
     merge_sort(arr)
-    assert arr == [11, 12, 22, 25, 34, 64, 90]
-
-def test_merge_sort_case2():
-    arr = []
-    merge_sort(arr)
-    assert arr == []
-
-def test_merge_sort_case3():
-    arr = [5, 4, 3, 2, 1]
-    merge_sort(arr)
-    assert arr == [1, 2, 3, 4, 5]
+    assert arr == esperado
 
 # Quick Sort Tests
-def test_quick_sort_case1():
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    assert quick_sort(arr) == [11, 12, 22, 25, 34, 64, 90]
-
-def test_quick_sort_case2():
-    arr = []
-    assert quick_sort(arr) == []
-
-def test_quick_sort_case3():
-    arr = [5, 4, 3, 2, 1]
-    assert quick_sort(arr) == [1, 2, 3, 4, 5]
+@pytest.mark.parametrize("arr, esperado", [
+    ([64, 34, 25, 12, 22, 11, 90], [11, 12, 22, 25, 34, 64, 90]),
+    ([], []),
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])
+])
+def test_quick_sort(arr, esperado):
+    assert quick_sort(arr) == esperado
